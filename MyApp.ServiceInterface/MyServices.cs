eac72d483034a747d4747333043f4235e3b1015d
Demis Bellot <demis.bellot@gmail.com>
2017-11-02 02:02:51 -0400

    Add initial .NET 4.5 react-app project

---
+++ MyApp.ServiceInterface/MyServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ServiceStack;
+using ServiceStack.Templates;
+using ServiceStack.DataAnnotations;
+using MyApp.ServiceModel;
+
+namespace MyApp.ServiceInterface
+{
+    [Exclude(Feature.Metadata)]
+    [FallbackRoute("/{PathInfo*}")]
+    public class FallbackForClientRoutes
+    {
+        public string PathInfo { get; set; }
+    }
+
+    public class MyServices : Service
+    {
+        //Return index.html for unmatched requests so routing is handled on client
+        public object Any(FallbackForClientRoutes request) => 
+            new PageResult(Request.GetPage("/"));
+            //new HttpResult(VirtualFileSources.GetFile("index.html")); //Use instead if index.html is just static HTML
+
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
