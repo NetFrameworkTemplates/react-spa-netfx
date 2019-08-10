3c9ae8e01109f35ca4ffa2b2696ea8fab685f75f
Demis Bellot <demis.bellot@gmail.com>
2019-08-10 04:03:53 -0400

    Upgrade to the latest version

---
+++ MyApp/Configure.Ui.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+
+namespace MyApp
+{
+    public class ConfigureUi : IConfigureAppHost
+    {
+        public void Configure(IAppHost appHost)
+        {
+            // if wwwroot/ is empty, build Client App with 'npm run build'
+            var svgDir = appHost.RootDirectory.GetDirectory("/svg") ?? appHost.ContentRootDirectory.GetDirectory("/public/svg"); 
+            if (svgDir != null)
+            {
+                Svg.Load(svgDir);
+            }
+
+            View.NavItems.AddRange(new List<NavItem>
+            {
+                new NavItem { Href = "/", Label = "Home", Exact = true },
+                new NavItem { Href = "/about", Label = "About" },
+                new NavItem { Href = "/login", Label = "Sign In", Hide = "auth" },
+                new NavItem { Href = "/profile", Label = "Profile", Show = "auth" },
+                new NavItem { Href = "/admin", Label = "Admin", Show = "role:Admin" },
+            });
+        }
+    }
+}
+
